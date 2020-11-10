e542ceab8bdc39e830562d126181bd10261f457e
rodriguesRodrigo <rrscotti@hotmail.com>
2020-11-10 00:10:33 -0300

    Login and logout

---
+++ lib/app/shared/auth/auth_controller.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:fluxo_caixa/app/shared/auth/repositories/auth_repository_interface.dart';
+import 'package:mobx/mobx.dart';
+part 'auth_controller.g.dart';
+
+class AuthController = _AuthControllerBase with _$AuthController;
+
+abstract class _AuthControllerBase with Store {
+
+  final IAuthRepository _authRepository = Modular.get();
+
+  @observable
+  User user;
+
+  @action
+  setUser(User value) => user = value;
+
+  _AuthControllerBase() {
+    _authRepository.getUser().then(setUser);
+  }
+
+  @action
+  Future loginWithGoogle() async {
+    user = await _authRepository.getGoogleLogin();
+  }
+
+  @action
+  Future logOut() {
+    return _authRepository.getLogout();
+  }
+
+}
