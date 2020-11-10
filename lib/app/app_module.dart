5ad1a67df3d4408b1b55af008b596fcc496d2ef6
rodriguesRodrigo <rrscotti@hotmail.com>
2020-11-10 00:17:56 -0300

    Route Module home and login

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,3 +1,8 @@
+import 'package:fluxo_caixa/app/modules/login/login_module.dart';
+import 'package:fluxo_caixa/app/shared/auth/auth_controller.dart';
+import 'package:fluxo_caixa/app/shared/auth/repositories/auth_repository_interface.dart';
+import 'package:fluxo_caixa/app/shared/auth/repositories/auth_repository.dart';
+
 import 'app_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/material.dart';
@@ -7,12 +12,16 @@ import 'package:fluxo_caixa/app/modules/home/home_module.dart';
 class AppModule extends MainModule {
   @override
   List<Bind> get binds => [
-        $AppController,
+        // $AppController,
+        Bind((i) => AppController),
+        Bind<IAuthRepository>((i) => AuthRepository()),        
+        Bind((i) => AuthController()),
       ];
 
   @override
   List<ModularRouter> get routers => [
-        ModularRouter(Modular.initialRoute, module: HomeModule()),
+        ModularRouter('/', module: LoginModule()),
+        ModularRouter('/home', module: HomeModule()),
       ];
 
   @override
