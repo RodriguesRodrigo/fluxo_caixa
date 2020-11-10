8f21432a50d0f0e9bb3929513f4575af05d8e16a
rodriguesRodrigo <rrscotti@hotmail.com>
2020-11-10 00:09:14 -0300

    Creating authentication interface and implementation

---
+++ lib/app/shared/auth/repositories/auth_repository.dart
@@ -0,0 +1,55 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fluxo_caixa/app/shared/auth/repositories/auth_repository_interface.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthRepository implements IAuthRepository {
+
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  Future getEmailPasswordLogin() {
+    // TODO: implement getEmailPasswordLogin
+    throw UnimplementedError();
+  }
+
+  @override
+  Future getFacebookLogin() {
+    // TODO: implement getFacebookLogin
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<User> getGoogleLogin() async {
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    final User user = (await _auth.signInWithCredential(credential)).user;
+    print('signed in ' + user.displayName);
+
+    return user;
+  }
+
+  @override
+  Future<String> getToken() {
+    // TODO: implement getToken
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<User> getUser() async {
+    final user = _auth.currentUser;
+    return user;
+  }
+
+  @override
+  Future getLogout() {
+    return _auth.signOut();
+  }
+
+}
