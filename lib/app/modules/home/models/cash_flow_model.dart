ba431c927a66fd6803b854cb9dd951132f684d45
rodriguesRodrigo <rrscotti@hotmail.com>
2020-11-19 00:34:14 -0300

    Add new colluns to cash flow model

--- lib/app/modules/home/models/cash_flow_model.dart
+++ lib/app/modules/home/models/cash_flow_model.dart
@@ -1,31 +1,83 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:fluxo_caixa/app/shared/auth/auth_controller.dart';
 
 class CashFlowModel {
   // TODO:
   // Order by date
 
-  String title;
-  bool check;
+  AuthController auth = Modular.get();
+
+  String userName;
+  String description;
+  String observation;
+  String type;
+  String paymentType;
+  String value;
+  Timestamp createdAt;
+  Timestamp paymentDate;
   DocumentReference reference;
 
-  CashFlowModel({this.title = '', this.check = false, this.reference});
+  CashFlowModel({
+    this.userName = '',
+    this.description = '',
+    this.observation = '',
+    this.type = '',
+    this.paymentType = '',
+    this.value = '',
+    this.createdAt,
+    this.paymentDate,
+    this.reference
+  });
 
   factory CashFlowModel.fromDocument(DocumentSnapshot doc) {
-    return CashFlowModel(title: doc['title'], check: doc['check'], reference: doc.reference);
+    return CashFlowModel(
+      userName: doc['userName'],
+      description: doc['description'],
+      observation: doc['observation'],
+      type: doc['type'],
+      paymentType: doc['paymentType'],
+      value: doc['value'],
+      createdAt: doc['createdAt'],
+      paymentDate: doc['paymentDate'],
+      reference: doc.reference
+    );
   }
 
   Future save() async {
+    userName = auth.user.email;
+
     if (reference == null) {
+
+      createdAt = new Timestamp.now() ?? createdAt == null;
+      paymentDate = new Timestamp.now() ?? paymentDate == null;
+
       reference = await FirebaseFirestore.instance
         .collection('cash_flux')
-        .add({'title': title, 'check': check,});
+        .add({
+          'description': description,
+          'observation': observation,
+          'value': value,
+          'type': type,
+          'paymentType': paymentType,
+          'createdAt': createdAt,
+          'paymentDate': paymentDate,
+          'userName': userName,
+        });
     }
     else {
       reference.update({
-        'title': title,
-        'check': check,
+        'description': description,
+        'observation': observation,
+        'value': value,
+        'type': type,
+        'paymentType': paymentType,
+        'createdAt': createdAt,
+        'paymentDate': paymentDate,
+        'userName': userName,
       });
     }
+    print('não salvei hue');
   }
 
   Future delete() {
