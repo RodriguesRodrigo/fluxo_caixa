cab225a25daccf9d1634e0faabbd160fcf38a277
rodriguesRodrigo <rrscotti@hotmail.com>
2020-11-22 19:55:00 -0300

    change class CashFlowRepository to FirestoreRepository

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:fluxo_caixa/app/modules/home/repositories/cash_flow_repository.dart';
-import 'package:fluxo_caixa/app/modules/home/repositories/cash_flow_repository_interface.dart';
+import 'package:fluxo_caixa/app/modules/home/repositories/firestore_repository.dart';
+import 'package:fluxo_caixa/app/modules/home/repositories/firestore_repository_interface.dart';
 
 import 'home_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -11,7 +11,7 @@ class HomeModule extends ChildModule {
   @override
   List<Bind> get binds => [
         Bind((i) => HomeController(i.get())),
-        Bind<ICashFlowRepository>((i) => CashFlowRepository(FirebaseFirestore.instance)),
+        Bind<IFirestoreRepository>((i) => FirestoreRepository(FirebaseFirestore.instance)),
       ];
 
   @override
