65a9a137a5e0e26e82e1665a3de0a8d2a6e9fb57
rodriguesRodrigo <rrscotti@hotmail.com>
2020-11-22 19:41:12 -0300

    create money transaction model

---
+++ lib/app/modules/home/models/money_transaction_model.dart
@@ -0,0 +1,49 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:fluxo_caixa/app/shared/auth/auth_controller.dart';
+
+class MoneyTransactionModel {
+
+  AuthController auth = Modular.get();
+
+  String userName;
+  String value;
+  DocumentReference reference;
+
+  MoneyTransactionModel({
+    this.userName = '',
+    this.value = '',
+    this.reference
+  });
+
+  factory MoneyTransactionModel.fromDocument(DocumentSnapshot doc) {
+    return MoneyTransactionModel(
+      userName: doc['userName'],
+      value: doc['value'],
+      reference: doc.reference
+    );
+  }
+
+  Future save() async {
+    userName = auth.user.email;
+
+    if (reference == null) {
+      reference = await FirebaseFirestore.instance
+        .collection('money_transaction')
+        .add({
+          'userName': userName,
+          'value': value,
+        });
+    }
+    else {
+      reference.update({
+        'userName': userName,
+        'value': value,
+      });
+    }
+  }
+
+  Future delete() {
+    return reference.delete();
+  }
+}
