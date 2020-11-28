8826db51fd8febc3d2b378e43c034934ce3dbacd
rodriguesRodrigo <rrscotti@hotmail.com>
2020-11-28 00:42:55 -0300

    login and redirect to createMoney when create a new user

--- lib/app/modules/create_user/create_user_controller.dart
+++ lib/app/modules/create_user/create_user_controller.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fluxo_caixa/app/modules/create_user/models/user_model.dart';
+import 'package:fluxo_caixa/app/shared/auth/auth_controller.dart';
 import 'package:mobx/mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -9,4 +11,20 @@ class CreateUserController = _CreateUserControllerBase
     with _$CreateUserController;
 
 abstract class _CreateUserControllerBase with Store {
+
+  AuthController auth = Modular.get();
+
+  @action
+  Future loginWithFirebase(email, password) async {
+    try {
+      await auth.loginEmailPassword(email, password);
+      Modular.to.pushReplacementNamed('/createMoney');
+    }
+    on FirebaseAuthException catch(e) {
+      print(e.code);
+    }
+    catch(e) {
+      print(e.code); 
+    }
+  }
 }
--- lib/app/modules/create_user/create_user_controller.g.dart
+++ lib/app/modules/create_user/create_user_controller.g.dart
@@ -19,6 +19,15 @@ final $CreateUserController = BindInject(
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$CreateUserController on _CreateUserControllerBase, Store {
+  final _$loginWithFirebaseAsyncAction =
+      AsyncAction('_CreateUserControllerBase.loginWithFirebase');
+
+  @override
+  Future<dynamic> loginWithFirebase(dynamic email, dynamic password) {
+    return _$loginWithFirebaseAsyncAction
+        .run(() => super.loginWithFirebase(email, password));
+  }
+
   @override
   String toString() {
     return '''
